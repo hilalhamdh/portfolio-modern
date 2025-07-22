69b1e9ea1d113f15c58388fe9b63b176e5cbe736
generusngasinan@gmail.com <hilalhamdihafit@gmail.com>
2025-07-22 14:59:55 +0800

    add file

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -31,16 +31,9 @@ const Service = () => {
         <div className="px-3 w-full border-[1px] mx-2 mt-[-2px]"></div>
         <div className="w-[20px] h-[7px] bg-green-600 rounded-full "></div>
       </div>
-      <div className="flex flex-col mt-16 ">
+      <div className="flex flex-col items-center justify-center mt-16 ">
         {servicesList.map((str) => (
           <div key={str.id} className="text-center   place-items-center gap-6">
-            <div className="bg-gray-200 rounded-full w-[90px] h-[90px]">
-              <img
-                src={str.logo}
-                alt="image"
-                className="h-[90px] w-[90px] p-5 hover:scale-110 transition-all ease-in-out cursor-pointer"
-              />
-            </div>
             <h2 className="font-bold mt-5">{str.title}</h2>
             <h2 className="text-gray-500 mx-3 md:mx-1">{str.desc}</h2>
             <IoArrowForwardCircle className="text-2xl text-green-500 hover:scale-110 ease-in-out transition-all my-3" />
