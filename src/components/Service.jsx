a5f419f10e52c276d3c34393178810d522d26b87
generusngasinan@gmail.com <hilalhamdihafit@gmail.com>
2025-07-22 14:23:00 +0800

    add file

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -31,21 +31,26 @@ const Service = () => {
         <div className="px-3 w-full border-[1px] mx-2 mt-[-2px]"></div>
         <div className="w-[20px] h-[7px] bg-green-600 rounded-full "></div>
       </div>
-      <div className="flex flex-col md:grid grid-cols-1 md:grid-cols-3 mt-16 ">
-        {servicesList.map((str) => (
-          <div key={str.id} className="text-center   place-items-center gap-6">
-            <div className="bg-gray-200 rounded-full w-[90px] h-[90px]">
-              <img
-                src={str.logo}
-                alt="image"
-                className="h-[90px] w-[90px] p-5 hover:scale-110 transition-all ease-in-out cursor-pointer"
-              />
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 mt-16 ">
+          {servicesList.map((str) => (
+            <div
+              key={str.id}
+              className="text-center   place-items-center gap-6"
+            >
+              <div className="bg-gray-200 rounded-full w-[90px] h-[90px]">
+                <img
+                  src={str.logo}
+                  alt="image"
+                  className="h-[90px] w-[90px] p-5 hover:scale-110 transition-all ease-in-out cursor-pointer"
+                />
+              </div>
+              <h2 className="font-bold mt-5">{str.title}</h2>
+              <h2 className="text-gray-500 mx-3 md:mx-1">{str.desc}</h2>
+              <IoArrowForwardCircle className="text-2xl text-green-500 hover:scale-110 ease-in-out transition-all my-3" />
             </div>
-            <h2 className="font-bold mt-5">{str.title}</h2>
-            <h2 className="text-gray-500 mx-3 md:mx-1">{str.desc}</h2>
-            <IoArrowForwardCircle className="text-2xl text-green-500 hover:scale-110 ease-in-out transition-all my-3" />
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
